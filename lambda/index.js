a67e1be654068ade1c9b3cda2d74b9129f6d20f3
Alex Marcucci <amarcuccis@Alexs-MBP.fibertel.com.ar>
2020-04-26 23:22:15 -0300

    added general status intent handler

--- lambda/index.js
+++ lambda/index.js
@@ -12,7 +12,7 @@
 const Alexa = require('ask-sdk-core');
 
 // ASK SDK adapter to connecto to Amazon S3
-const persistenceAdapter = require('ask-sdk-s3-persistence-adapter');
+//const persistenceAdapter = require('ask-sdk-s3-persistence-adapter');
 
 // i18n library dependency, we use it below in a localisation interceptor
 const i18n = require('i18next');
@@ -24,7 +24,16 @@ const languageStrings = require('./languageStrings');
 const https = require('https');
 
 // Filled with the successful response of the API Transporte
-let apiResponse;
+//let apiResponse;
+
+//MOCK 5 with alerts: 4 the same, 1 diff. 1 line normal
+let apiResponse = '{"header":{"gtfs_realtime_version":"2.0","incrementality":0,"timestamp":1587841510},"entity":[{"id":"Alert_LineaA","is_deleted":false,"trip_update":null,"vehicle":null,"alert":{"active_period":[],"informed_entity":[{"agency_id":"","route_id":"LineaA","route_type":0,"trip":null,"stop_id":""}],"cause":12,"effect":6,"url":null,"header_text":{"translation":[{"text":"Por protocolo de prevención: circula con un servicio especial. Info www.metrovias.com.ar","language":"es"}]},"description_text":{"translation":[{"text":"Por protocolo de prevención: circula con un servicio especial. Info www.metrovias.com.ar","language":"es"}]}}},{"id":"Alert_LineaB","is_deleted":false,"trip_update":null,"vehicle":null,"alert":{"active_period":[],"informed_entity":[{"agency_id":"","route_id":"LineaB","route_type":0,"trip":null,"stop_id":""}],"cause":12,"effect":6,"url":null,"header_text":{"translation":[{"text":"circula con servicio limitado en las estaciones Medrano y Alem.","language":"es"}]},"description_text":{"translation":[{"text":"circula con servicio limitado en las estaciones Medrano y Alem.","language":"es"}]}}},{"id":"Alert_LineaC","is_deleted":false,"trip_update":null,"vehicle":null,"alert":{"active_period":[],"informed_entity":[{"agency_id":"","route_id":"LineaC","route_type":0,"trip":null,"stop_id":""}],"cause":12,"effect":6,"url":null,"header_text":{"translation":[{"text":"Por protocolo de prevención: circula con un servicio especial. Info www.metrovias.com.ar","language":"es"}]},"description_text":{"translation":[{"text":"Por protocolo de prevención: circula con un servicio especial. Info www.metrovias.com.ar","language":"es"}]}}},{"id":"Alert_LineaE","is_deleted":false,"trip_update":null,"vehicle":null,"alert":{"active_period":[],"informed_entity":[{"agency_id":"","route_id":"LineaE","route_type":0,"trip":null,"stop_id":""}],"cause":12,"effect":6,"url":null,"header_text":{"translation":[{"text":"Por protocolo de prevención: circula con un servicio especial. Info www.metrovias.com.ar","language":"es"}]},"description_text":{"translation":[{"text":"Por protocolo de prevención: circula con un servicio especial. Info www.metrovias.com.ar","language":"es"}]}}},{"id":"Alert_LineaH","is_deleted":false,"trip_update":null,"vehicle":null,"alert":{"active_period":[],"informed_entity":[{"agency_id":"","route_id":"LineaH","route_type":0,"trip":null,"stop_id":""}],"cause":12,"effect":6,"url":null,"header_text":{"translation":[{"text":"Por protocolo de prevención: circula con un servicio especial. Info www.metrovias.com.ar","language":"es"}]},"description_text":{"translation":[{"text":"Por protocolo de prevención: circula con un servicio especial. Info www.metrovias.com.ar","language":"es"}]}}}]}';
+
+// MOCK no alerts
+//let apiResponse = '{"header":{"gtfs_realtime_version":"2.0","incrementality":0,"timestamp":1587841510},"entity":[]}';
+
+// MOCK 1 alert
+//let apiResponse = '{"header":{"gtfs_realtime_version":"2.0","incrementality":0,"timestamp":1587841510},"entity":[{"id":"Alert_LineaB","is_deleted":false,"trip_update":null,"vehicle":null,"alert":{"active_period":[],"informed_entity":[{"agency_id":"","route_id":"LineaB","route_type":0,"trip":null,"stop_id":""}],"cause":12,"effect":6,"url":null,"header_text":{"translation":[{"text":"circula con servicio limitado en las estaciones Medrano y Alem.","language":"es"}]},"description_text":{"translation":[{"text":"circula con servicio limitado en las estaciones Medrano y Alem.","language":"es"}]}}}]}';
 
 // If the HTTPS response is not successful or there is another problem, is marked true  
 let requestIsError = false;
@@ -80,12 +89,31 @@ const SingleStatusIntentHandler = {
             speakOutput = handlerInput.t('SINGLE_LINE_STATUS_RESPONSE_MSG', { line: line, lineStatus: lineStatus });
         }
         
-        const listenOutput = handlerInput.t('SINGLE_LINE_LISTEN_MSG');
+        const listenOutput = handlerInput.t('ASK_ANOTHER_INFO_MSG');
         
         return handlerInput.responseBuilder
             .speak(speakOutput)
             .reprompt(listenOutput)
-            .withShouldEndSession(requestIsError) // force the skill to close the session after confirming the birthday date
+            .withShouldEndSession(requestIsError)
+            .getResponse();
+    }
+};
+
+/**
+ * Handles GeneralStatusIntentHandler requests sent by Alexa (when a user asks for general Subte status)
+ */
+const GeneralStatusIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getIntentName(handlerInput.requestEnvelope) === 'GeneralStatusIntent';
+    },
+    async handle(handlerInput) {                
+        const speakOutput = getSubteGeneralStatus(handlerInput);
+        const listenOutput = handlerInput.t('ASK_ANOTHER_INFO_MSG');
+        
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(listenOutput)
+            .withShouldEndSession(false)
             .getResponse();
     }
 };
@@ -192,6 +220,7 @@ function doAPICallout() {
     });
 
 }
+
 /**
  * Returns the specific status description message from the incoming Subte Line
  * @param {*} inputLine The Identifier of the Subte Line
@@ -201,8 +230,10 @@ function getSubteStatus(inputLine, handlerInput) {
     let messageStatus='';
     console.log('= inputLine: '+inputLine);
     
-    //doAPICallout();
-    apiResponse = '{"header":{"gtfs_realtime_version":"2.0","incrementality":0,"timestamp":1587841510},"entity":[{"id":"Alert_LineaA","is_deleted":false,"trip_update":null,"vehicle":null,"alert":{"active_period":[],"informed_entity":[{"agency_id":"","route_id":"LineaA","route_type":0,"trip":null,"stop_id":""}],"cause":12,"effect":6,"url":null,"header_text":{"translation":[{"text":"Por protocolo de prevención: circula con un servicio especial. Info www.metrovias.com.ar","language":"es"}]},"description_text":{"translation":[{"text":"Por protocolo de prevención: circula con un servicio especial. Info www.metrovias.com.ar","language":"es"}]}}},{"id":"Alert_LineaB","is_deleted":false,"trip_update":null,"vehicle":null,"alert":{"active_period":[],"informed_entity":[{"agency_id":"","route_id":"LineaB","route_type":0,"trip":null,"stop_id":""}],"cause":12,"effect":6,"url":null,"header_text":{"translation":[{"text":"circula con servicio limitado en las estaciones Medrano y Alem.","language":"es"}]},"description_text":{"translation":[{"text":"circula con servicio limitado en las estaciones Medrano y Alem.","language":"es"}]}}},{"id":"Alert_LineaC","is_deleted":false,"trip_update":null,"vehicle":null,"alert":{"active_period":[],"informed_entity":[{"agency_id":"","route_id":"LineaC","route_type":0,"trip":null,"stop_id":""}],"cause":12,"effect":6,"url":null,"header_text":{"translation":[{"text":"Por protocolo de prevención: circula con un servicio especial. Info www.metrovias.com.ar","language":"es"}]},"description_text":{"translation":[{"text":"Por protocolo de prevención: circula con un servicio especial. Info www.metrovias.com.ar","language":"es"}]}}},{"id":"Alert_LineaE","is_deleted":false,"trip_update":null,"vehicle":null,"alert":{"active_period":[],"informed_entity":[{"agency_id":"","route_id":"LineaE","route_type":0,"trip":null,"stop_id":""}],"cause":12,"effect":6,"url":null,"header_text":{"translation":[{"text":"Por protocolo de prevención: circula con un servicio especial. Info www.metrovias.com.ar","language":"es"}]},"description_text":{"translation":[{"text":"Por protocolo de prevención: circula con un servicio especial. Info www.metrovias.com.ar","language":"es"}]}}},{"id":"Alert_LineaH","is_deleted":false,"trip_update":null,"vehicle":null,"alert":{"active_period":[],"informed_entity":[{"agency_id":"","route_id":"LineaH","route_type":0,"trip":null,"stop_id":""}],"cause":12,"effect":6,"url":null,"header_text":{"translation":[{"text":"Por protocolo de prevención: circula con un servicio especial. Info www.metrovias.com.ar","language":"es"}]},"description_text":{"translation":[{"text":"Por protocolo de prevención: circula con un servicio especial. Info www.metrovias.com.ar","language":"es"}]}}}]}';
+    //if(apiResponse != undefined){
+    //    doAPICallout();
+    //}
+    
     //requestIsError = true;
     
     if(inputLine != undefined && !requestIsError && apiResponse != undefined) {
@@ -234,6 +265,133 @@ function getSubteStatus(inputLine, handlerInput) {
     }
     return messageStatus;
 }
+
+function getSubteGeneralStatus(handlerInput) {
+    let messageStatus;
+    
+    if(apiResponse != undefined){
+        doAPICallout();
+    }
+
+    //requestIsError = true;
+
+    if(!requestIsError && apiResponse != undefined) {
+        console.log('== apiResponse: '+JSON.parse(apiResponse));
+
+        let lang = Alexa.getLocale(handlerInput.requestEnvelope).split('-')[0];
+        console.log('== lang: '+lang);
+        
+        let parsedResponse = JSON.parse(apiResponse);
+        let allAlerts = parsedResponse.entity;
+        let lineAlertsByMessage = new Map();
+        const subteLinesQuantity = 6;
+        const alertsQuantity = allAlerts.length;
+        let allLinesAlertsDesc = '';
+        let allLinesHaveAlerts = alertsQuantity === subteLinesQuantity;
+        let onlyOneLineWithAlert = alertsQuantity === 1;
+        let onlyOneLineNormal = alertsQuantity === 5;
+        
+        console.log('== allAlerts: '+allAlerts);
+
+        for(var i = 0; i < alertsQuantity; i++) {
+            let line = allAlerts[i].id;
+            let alertMessage = allAlerts[i].alert.description_text.translation[0].text;
+            console.log('== line: '+line);
+
+            if(!lineAlertsByMessage.has(alertMessage)) {
+                lineAlertsByMessage.set(alertMessage,[]);
+            }
+            let msgLinesList = lineAlertsByMessage.get(alertMessage);
+            msgLinesList.push(line);
+            lineAlertsByMessage.set(alertMessage,msgLinesList);
+
+            console.log('=== new linea in map: ' + alertMessage + ' : ' + line);
+        }
+
+        if(lineAlertsByMessage.size === 0) {
+            messageStatus = handlerInput.t('GRAL_STATUS_ALL_LINES_NORMAL_MSG');
+
+        } else {
+
+            for(let [lineAlert, lines] of lineAlertsByMessage.entries()) {
+
+                console.log('==== inside lineAlertsByMessage: '+lineAlertsByMessage);
+                console.log('==== lineAlert: '+lineAlert);
+                console.log('==== lines: '+lines);
+
+                let linesWithSameAlert = lines.length;
+
+                console.log('==== linesWithSameAlert: '+lines.length);
+                console.log('==== lines.length === 1: '+lines.length == 1);
+                console.log('==== linesWithSameAlert > 1: '+lines.length > 1);
+
+                let linesListMsg = '';
+                for(let line of lines) {
+                    lineWord = line.slice(line.length-1);
+                    linesListMsg += lineWord + ', ';
+                }
+
+                if(linesWithSameAlert === 1) {
+
+                    console.log('===== 1 allLinesAlertsDesc: '+SINGLE_PHRASE_1_GRAL_MSG_TRAD.get(lang) + linesListMsg + SINGLE_PHRASE_2_GRAL_MSG_TRAD.get(lang) +
+                    lineAlert + '. ');
+
+                    allLinesAlertsDesc += SINGLE_PHRASE_1_GRAL_MSG_TRAD.get(lang) + linesListMsg + SINGLE_PHRASE_2_GRAL_MSG_TRAD.get(lang) +
+                        lineAlert + '. ';
+
+                }
+                if (linesWithSameAlert > 1) {
+
+                    console.log('===== many allLinesAlertsDesc: '+SINGLE_PHRASE_1_GRAL_MSG_TRAD.get(lang) + linesListMsg + SINGLE_PHRASE_2_GRAL_MSG_TRAD.get(lang) +
+                    lineAlert + '. ');
+
+                    allLinesAlertsDesc += PLURAL_PHRASE_1_GRAL_MSG_TRAD.get(lang) + linesListMsg + PLURAL_PHRASE_2_GRAL_MSG_TRAD.get(lang) +
+                        lineAlert + '. ';
+                }
+            }
+
+            if(allLinesHaveAlerts) {
+                messageStatus = handlerInput.t('GRAL_STATUS_ALL_LINES_WITH_ALERT') +
+                    handlerInput.t('GRAL_STATUS_PRESENTATION_MSG', { allLineAlerts: allLinesAlertsDesc });
+
+            } else if(onlyOneLineNormal) {
+                messageStatus = handlerInput.t('GRAL_STATUS_ONE_LINE_NORMAL') +
+                    handlerInput.t('GRAL_STATUS_PRESENTATION_MSG', { allLineAlerts: allLinesAlertsDesc });
+
+            } else if (onlyOneLineWithAlert) {
+                messageStatus = handlerInput.t('GRAL_STATUS_ONE_LINE_WITH_ALERT') +
+                    handlerInput.t('GRAL_STATUS_PRESENTATION_MSG', { allLineAlerts: allLinesAlertsDesc });
+            }
+        }
+        console.log('==== allLinesAlertsDesc: '+allLinesAlertsDesc);
+        console.log('==== mensaje: '+messageStatus);
+
+    } else {
+        messageStatus = handlerInput.t('REQUEST_ERROR_MSG');
+        requestIsError = true;
+    }
+    return messageStatus;
+}
+
+const SINGLE_PHRASE_1_GRAL_MSG_TRAD = new Map([
+    ['es' , 'La línea '],
+    ['en' , 'Line ']
+]);
+
+const SINGLE_PHRASE_2_GRAL_MSG_TRAD = new Map([
+    ['es' , ' se encuentra con una alerta. '],
+    ['en' , ' have an alert. ']
+]);
+
+const PLURAL_PHRASE_1_GRAL_MSG_TRAD = new Map([
+    ['es' , 'Las líneas '],
+    ['en' , 'Lines ']
+]);
+
+const PLURAL_PHRASE_2_GRAL_MSG_TRAD = new Map([
+    ['es' , ' se encuentran con una alerta. '],
+    ['en' , ' has an alert. ']
+]);
 
 /////////////////////////////////
 // Interceptors Definition
@@ -286,6 +444,7 @@ exports.handler = Alexa.SkillBuilders.custom()
     .addRequestHandlers(
         LaunchRequestHandler,
         SingleStatusIntentHandler,
+        GeneralStatusIntentHandler,
         HelpIntentHandler,
         CancelAndStopIntentHandler,
         SessionEndedRequestHandler)
